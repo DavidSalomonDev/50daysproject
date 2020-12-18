aa4f710a6e11f38168575831311251347444bbf6
David Salomón <davidsvmartinez@gmail.com>
2020-12-18 07:48:46 -0600

    11. Event KeyCode

--- 10_dad_jokes/script.js
+++ 10_dad_jokes/script.js
@@ -5,15 +5,32 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+//Using .then()
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     };
+//     fetch('https://icanhazdadjoke.com/', config)
+//     .then(res => res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }
+
+// Using async/await
+async function generateJoke() {
     const config = {
         headers: {
-            'Accept': 'application/json'
-        }
-    };
-    fetch('https://icanhazdadjoke.com/', config)
-    .then(res => res.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    })
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+
 }
